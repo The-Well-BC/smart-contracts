e78085917999ce378af280d906bfe9db557cadf5
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-04-19 11:46:49 +0100

    Update truffle config

--- collab-nft/truffle-config.js
+++ collab-nft/truffle-config.js
@@ -1,29 +1,4 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -44,8 +19,10 @@ module.exports = {
         },
         ropsten: {
             provider: function() {
-                // return new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${ process.env.INFURA_ROPSTEN_API_KEY }`, 2)
-                return new HDWalletProvider(process.env.WALLET_PRIVATE_KEY, `https://ropsten.infura.io/v3/${ process.env.INFURA_ROPSTEN_API_KEY }`)
+                return new HDWalletProvider({
+                    mnemonic: process.env.MNEMONIC,
+                    providerOrUrl: `https://ropsten.infura.io/v3/${ process.env.INFURA_ROPSTEN_API_KEY }`
+                })
             },
             network_id: 3,
             gas: 8000000,
