7c8c9f6b65d74d3f0721783221c11d32f9f89d38
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-05-05 20:38:48 +0100

    Update Well to ERC20Minter token contract

--- contracts/well.sol
+++ contracts/well.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20Minter.sol";
+// import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract Well is ERC20 {
-    constructor() ERC20('Well Token', 'WELL') {
+contract Well is ERC20Minter {
+    string private _name;
+
+    constructor() ERC20Minter('Well', 'WELL'){
+        _name = 'Well';
     }
 }
