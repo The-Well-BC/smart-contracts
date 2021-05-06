2e4398a9c34f28a5fd97b96f2389a7d96f249133
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-05-06 16:29:16 +0100

    Add migration for Crowdsale, the token contracts

---
+++ migrations/2_crowdsale_migration.js
@@ -0,0 +1,27 @@
+const WhitelistCrowdsale = artifacts.require('WhitelistCrowdsale');
+const Well = artifacts.require('Well');
+const Fresh = artifacts.require('Fresh');
+
+module.exports = function(deployer) {
+    // Deploy the tokens
+    return Promise.all([
+        deployer.deploy(Well),
+        deployer.deploy(Fresh)
+    ])
+    // Deploy Crowdsale contract
+    // TODO: Replace address
+    .then(() => {
+        return deployer.deploy( WhitelistCrowdsale,
+            4, '0x750e13021FD1c43E617e09CC998Ef90Ea1b98DC4', Well.address, Fresh.address
+        );
+    })
+    .then(() => Promise.all([ Well.deployed(), Fresh.deployed() ]))
+    .then(res => {
+        const [ well, fresh ] = res;
+
+        return Promise.all([
+            well.grantMinterRole(WhitelistCrowdsale.address),
+            fresh.grantMinterRole(WhitelistCrowdsale.address)
+        ]);
+    });
+}
