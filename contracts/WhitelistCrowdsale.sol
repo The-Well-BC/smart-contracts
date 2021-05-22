347ecaf11fdb4246c6ce96d8718452c2622bf1d0
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-05-23 00:14:02 +0100

    Cleanup - WhitelistCrowdsale

--- contracts/WhitelistCrowdsale.sol
+++ contracts/WhitelistCrowdsale.sol
@@ -2,21 +2,13 @@
 
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// TODO: Handle sale of both WELL and mint tokens to collectors.
 contract WhitelistCrowdsale is Context {
     // Whitelisted buyers
     mapping(address => bool) public whitelist;
 
     // Owner address
     address private owner;
-
-    /**
-     * @dev The rate is the conversion between wei and the smallest and indivisible
-     * token unit. So, if you are using a rate of 1 with a ERC20Detailed token
-     * with 3 decimals called TOK, 1 wei will give you 1 unit, or 0.001 TOK.
-     */
 
     constructor() {
         owner = msg.sender;
@@ -27,10 +19,6 @@ contract WhitelistCrowdsale is Context {
          _;
     }
 
-    /* Whitelist Functions */
-    /**
-      * @dev Checks that buyer is whitelisted
-      */
     modifier isWhitelisted(address beneficiary_) {
         require(whitelist[beneficiary_] == true, 'Crowdsale: Address not allowed to buy token');
         _;
