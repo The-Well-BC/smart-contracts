f3f8368cae06bd2730d6931fdb92e8eb0b67870e
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-09-13 18:02:45 +0100

    Update SPDX licenses

--- contracts/theWellNFT.sol
+++ contracts/theWellNFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.4;
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
